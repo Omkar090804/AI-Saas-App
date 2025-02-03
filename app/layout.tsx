6309ddacef9bfe83f6ae7cdb5e0806d7002af83f
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-02-03 11:33:47 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils";                          //utils file created by Shadcn
 import { ClerkProvider } from "@clerk/nextjs";
 
-import "./globals.css";
+import "./globals.css";                                   //helper class for desigining 
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
