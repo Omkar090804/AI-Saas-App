400d65880716fd835b93304173a93212de95dc56
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-02-06 12:21:35 +0530

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,8 @@
 
 //rafce shortcut for creating react template if ES7+ extension is present
 
+//layout basically exports the children which allows to add different UI's to the differnt routes like nav bar,footer etc.
+
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
